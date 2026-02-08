c6634f13e057b34694f95050c25275e9c93b2e3f
Nanda Kishor S <kishorsn999@gmail.com>
2026-02-08 18:55:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,35 +6,39 @@ fetch("data.json")
   .then(data => dataset = data);
 
 function initScene() {
+
+  document.getElementById("viewer").innerHTML = "";
+
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(
-    75, 600 / 400, 0.1, 1000
-  );
+  camera = new THREE.PerspectiveCamera(75, 600/400, 0.1, 1000);
   camera.position.z = 3;
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(600, 400);
 
-  document.getElementById("viewer").innerHTML = "";
   document.getElementById("viewer").appendChild(renderer.domElement);
 
   const light = new THREE.PointLight(0xffffff);
-  light.position.set(5, 5, 5);
+  light.position.set(5,5,5);
   scene.add(light);
 }
 
 function createModel(type) {
+
   let geometry;
 
   if (type === "cube")
     geometry = new THREE.BoxGeometry();
+
   else if (type === "sphere")
-    geometry = new THREE.SphereGeometry();
+    geometry = new THREE.SphereGeometry(0.8, 32, 32);
+
   else if (type === "cone")
-    geometry = new THREE.ConeGeometry();
+    geometry = new THREE.ConeGeometry(0.8, 1.5, 32);
+
   else if (type === "pyramid")
-    geometry = new THREE.ConeGeometry(1,1,4);
+    geometry = new THREE.ConeGeometry(1, 1.5, 4);
 
   const material = new THREE.MeshStandardMaterial({color: 0x00ffcc});
   mesh = new THREE.Mesh(geometry, material);
@@ -53,8 +57,9 @@ function analyze(task) {
 
   let filtered = dataset.filter(d => d.task_label === task);
 
-  if(task === "random"){
-    filtered = dataset;
+  if(filtered.length === 0){
+    alert("No data found");
+    return;
   }
 
   const row = filtered[Math.floor(Math.random() * filtered.length)];
@@ -64,13 +69,12 @@ function analyze(task) {
   animate();
 
   document.getElementById("dataOutput").innerHTML = `
+    Subject ID: ${row.subject_id}<br>
+    Trial ID: ${row.trial_id}<br>
+    Task: ${row.task_label}<br>
     C3 Alpha: ${row.C3_alpha}<br>
     Cz Alpha: ${row.Cz_alpha}<br>
     C4 Alpha: ${row.C4_alpha}<br>
-    C3 Beta: ${row.C3_beta}<br>
-    Cz Beta: ${row.Cz_beta}<br>
-    C4 Beta: ${row.C4_beta}<br>
     Feature Energy: ${row.feature_energy}
   `;
 }
-
